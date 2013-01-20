bf892ed024088d803b47c51af45ea24693eddfeb
Bart Aelterman <bart.aelterman@gmail.com>
2013-01-20 16:56:05 +0100

    Update Test_Vogel + added Test_Zwerm

--- Test_bird_swarm.py
+++ Test_bird_swarm.py
@@ -3,7 +3,6 @@ import unittest
 
 class Test_Vogel(unittest.TestCase):
     def test_init(self):
-        master = Tk()
         testcanvas = Canvas(master,width=800,height=400)
         testcanvas.pack()
         testvogel = vogel(testcanvas)
@@ -13,7 +12,7 @@ class Test_Locatie(unittest.TestCase):
     g_groottescherm = 400
     def test_init(self):
         loc = locatie()
-	pass
+        pass
 
     def test_x_larger_than_0(self):
         loc = locatie()
@@ -30,3 +29,17 @@ class Test_Locatie(unittest.TestCase):
     def test_y_smaller_than_scherm(self):
         loc = locatie()
         self.assertTrue(loc.geefy() < g_groottescherm)
+
+class Test_Zwerm(unittest.TestCase):
+    def test_init(self):
+        master = Tk()
+        zw = zwerm(1, master)
+        self.assertEqual(zw.geefaantalvogels(), 1)
+
+    """
+    def test_geefmidden(self):
+        master = Tk()
+        zw = zwerm(1, master)
+        vogels = zw.__vv
+        self.assertEqual(len(vogels), 1)
+    """
