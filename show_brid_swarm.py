640ec6ef80bcb6ab75bfbd488e0d5529cca53ef2
Bart Aelterman <bart.aelterman@gmail.com>
2013-01-20 15:31:40 +0100

    extracted main script code

---
+++ show_brid_swarm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import bird_swarm
+from Tkinter import *
+
+#GLOBALVARS
+g_aantalvogels = 3
+g_groottescherm = 1000
+g_groottevogel = 6
+g_flapperafstand = 50
+g_aantalticks = 10
+
+master = Tk()
+z = bird_swarm.zwerm(g_aantalvogels,master)
+#z.bepaalmidden()
+
+for t in range(1,g_aantalticks):
+    print('ronde')
+    print(t)
+    z.vlieg()
+#    time.sleep(0.50)
+#master.mainloop()
