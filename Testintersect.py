fe34aad4ed790d440d789a429d8d790f2eea1313
Bart Aelterman <bart.aelterman@gmail.com>
2013-01-23 08:59:03 +0100

    test getDistance added

--- Testintersect.py
+++ Testintersect.py
@@ -29,3 +29,11 @@ class Test_intersect(unittest.TestCase):
 	expected_result = sorted([[result1_x, result1_y], [result2_x, result2_y]]) 
 	result = sorted(intersect.getIntersectingPoints(px, py, radius, lpx, lpy))
 	self.assertEqual(expected_result, result)
+
+    def testdistance(self):
+        point1_x = -2
+	point1_y = 1
+	point2_x = 1
+	point2_y = 5
+	expected_distance = 5
+	self.assertEqual(expected_distance, intersect.distanceBetweenPoints(point1_x, point1_y, point2_x, point2_y))
