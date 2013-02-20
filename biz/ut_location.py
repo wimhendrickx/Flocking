8c3f5c2b1eff4db47acf666681a90911e354f944
Wim Hendrickx <hendrickx.wim@gmail.com>
2013-02-20 23:33:36 +0100

    ALL UT RUNNING

--- biz/ut_location.py
+++ biz/ut_location.py
@@ -1,30 +1,31 @@
 import unittest
 import location
 
+g_groottescherm = 400
+
 class Test_Locatie(unittest.TestCase):
-    g_groottescherm = 400
     def test_init(self):
-        loc = locatie()
+        loc = location.location()
         pass
 
     def test_x_larger_than_0(self):
-        loc = locatie()
-        self.assertTrue(0 < loc.geefx())
+        loc = location.location()
+        self.assertTrue(0 < loc.getX())
 
     def test_x_smaller_than_scherm(self):
-        loc = locatie()
-        self.assertTrue(loc.geefx() < g_groottescherm)
+        loc = location.location()
+        self.assertTrue(loc.getX() < g_groottescherm)
 
     def test_y_larger_than_0(self):
-        loc = locatie()
-        self.assertTrue(0 < loc.geefy())
+        loc = location.location()
+        self.assertTrue(0 < loc.getY())
 
     def test_y_smaller_than_scherm(self):
-        loc = locatie()
-        self.assertTrue(loc.geefy() < g_groottescherm)
+        loc = location.location()
+        self.assertTrue(loc.getY() < g_groottescherm)
 
     def test_division_x(self):
         setx = float(1)/2
         sety = 1
-        loc = locatie(x=setx, y=1)
-        self.assertTrue(loc.geefx(), 0.5)
+        loc = location.location(x=setx, y=1)
+        self.assertTrue(loc.getX(), 0.5)
